ffa15d57e5a3660b05a3b29d7f3d5649eb880e6e
Aleksander Kukhtin <kukhtin@radmirtech.com.ua>
2021-08-05 11:28:45 +0300

    #PR-1 create Entities

---
+++ src/main/java/com/akukhtin/testapplication/domain/Role.java
@@ -0,0 +1,25 @@
+package com.akukhtin.testapplication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    @Column(name = "role_name")
+    private String name;
+}
---
+++ src/main/java/com/akukhtin/testapplication/domain/User.java
@@ -0,0 +1,36 @@
+package com.akukhtin.testapplication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
